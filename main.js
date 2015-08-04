b5d62698240e4d60195766bc1e3533b0213d24fd
Mike Killion <killion.kgs@gmail.com>
2015-08-04 09:15:05 -0500

    Exclude Justin Rubinstein's events in filters

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-require(["esri/map", "esri/dijit/Scalebar", "dojo/domReady!"], function(Map,Scalebar) { 
+require(["esri/map", "esri/dijit/Scalebar", "dojo/domReady!"], function(Map,Scalebar) {
     var map = new Map("map-div", {
         center: [-98.5, 38.25],
         zoom: 7,
         basemap: "national-geographic",
         logo: false
     });
-    
+
     var scalebar = new Scalebar({
         map: map,
         scalebarUnit: "dual"
     });
-    
+
     earthquakeLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.kgs.ku.edu/arcgis/rest/services/CO2/seismic/MapServer");
     earthquakeLayer.setVisibleLayers([8]);
     map.addLayers([earthquakeLayer]);
@@ -24,7 +24,7 @@ function filterQuakes(year, mag) {
         if (mag !== "all") {
             def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and mag >=" + mag;
         } else {
-            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')";   
+            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')";
         }
     } else {
         if (mag !== "all") {
@@ -34,12 +34,12 @@ function filterQuakes(year, mag) {
         }
     }
 
-    earthquakeLayer.setLayerDefinitions(def);   
+    earthquakeLayer.setLayerDefinitions(def);
 }
 
 function filterQuakesRecent() {
     var def = [];
-    def[8] = "state = 'KS' and the_date = (select max(the_date) from earthquakes where state = 'KS')";
+    def[8] = "state = 'KS' and lower(net) <> 'ismpkansas' and the_date = (select max(the_date) from earthquakes where state = 'KS' and lower(net) <> 'ismpkansas'";
     earthquakeLayer.setLayerDefinitions(def);
 }
 
