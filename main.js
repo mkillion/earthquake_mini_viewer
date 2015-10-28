3caa8428ae52843da8ff8a862237f0c4d7b2eb06
Mike Killion <killion.kgs@gmail.com>
2015-10-28 09:21:16 -0500

    Fix filter issues related to events from ismpkansas net.

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ require(["esri/map", "esri/dijit/Scalebar", "dojo/domReady!"], function(Map,Scal
         scalebarUnit: "dual"
     });
 
-    earthquakeLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.kgs.ku.edu/arcgis/rest/services/CO2/seismic_1/MapServer");
-    earthquakeLayer.setVisibleLayers([8]);
-    map.addLayers([earthquakeLayer]);
+    earthquakesLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.kgs.ku.edu/arcgis/rest/services/CO2/seismic_1/MapServer");
+    earthquakesLayer.setVisibleLayers([8]);
+    map.addLayers([earthquakesLayer]);
 });
 
 function filterQuakes(year, mag) {
@@ -22,9 +22,9 @@ function filterQuakes(year, mag) {
 
     if (year !== "all") {
         if (mag !== "all") {
-            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and mag >=" + mag;
+            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and net in ('us', ' ', 'US') and mag >=" + mag;
         } else {
-            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')";
+            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and mag >= 2 and net in ('us', ' ', 'US')";
         }
     } else {
         if (mag !== "all") {
@@ -34,30 +34,30 @@ function filterQuakes(year, mag) {
         }
     }
 
-    earthquakeLayer.setLayerDefinitions(def);
+    earthquakesLayer.setLayerDefinitions(def);
 }
 
 function filterQuakesRecent() {
     var def = [];
-    def[8] = "state = 'KS' and lower(net) <> 'ismpkansas' and the_date = (select max(the_date) from earthquakes where state = 'KS' and lower(net) <> 'ismpkansas')";
-    earthquakeLayer.setLayerDefinitions(def);
+    def[8] = "state = 'KS' and mag >= 2 and net in ('us', ' ', 'US') and the_date = (select max(the_date) from earthquakes where state = 'KS' and mag >= 2 and net in ('us', ' ', 'US'))";
+    earthquakesLayer.setLayerDefinitions(def);
 }
 
 function filterQuakesDays(days) {
     var def = [];
 
     if (days !== "all") {
-        def[8] = "sysdate - the_date <= " + days;
+        def[8] = "sysdate - the_date <= " + days + " and mag >= 2 and net in ('us', ' ', 'US')";
     } else {
         def[8] = "";
     }
-    earthquakeLayer.setLayerDefinitions(def);
+    earthquakesLayer.setLayerDefinitions(def);
 }
 
 function clearQuakeFilter() {
     var def = [];
     def = "";
-    earthquakeLayer.setLayerDefinitions(def);
+    earthquakesLayer.setLayerDefinitions(def);
     days.options[0].selected="selected";
     mag.options[0].selected="selected";
     year.options[0].selected="selected";
