f4c1d890ff69c6482ff8a70d7472ee0346697edf
Mike Killion <killion.kgs@gmail.com>
2018-09-21 11:32:27 -0500

    Update to API 4.8, basic map displays.

--- main.js
+++ main.js
@@ -1,21 +1,111 @@
 
-require(["esri/map", "esri/dijit/Scalebar", "dojo/domReady!"], function(Map,Scalebar) {
-    var map = new Map("map-div", {
-        center: [-98.5, 38.25],
-        zoom: 7,
-        basemap: "national-geographic",
-        logo: false
-    });
+require([
+    "esri/Map",
+    "esri/widgets/ScaleBar",
+    "esri/views/MapView",
+    "esri/layers/TileLayer",
+    "esri/layers/MapImageLayer",
+    "esri/renderers/ClassBreaksRenderer",
+	"esri/symbols/SimpleMarkerSymbol",
+    "dojo/dom",
+    "dojo/domReady!"
+],
+function(
+    Map,
+    ScaleBar,
+    MapView,
+    TileLayer,
+    MapImageLayer,
+    ClassBreaksRenderer,
+    SimpleMarkerSymbol,
+    dom
+) {
+    var basemapLayer = new TileLayer( {url:"//services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer", id:"Base Map"} );
 
-    var scalebar = new Scalebar({
+    var quakesRenderer = new ClassBreaksRenderer( {
+		field: "magnitude"
+	} );
+	quakesRenderer.addClassBreakInfo( {
+  		minValue: 0,
+  		maxValue: 1,
+		label: "Less than 1.0",
+  		symbol: new SimpleMarkerSymbol( {
+    		style: "circle",
+    		size: 10,
+    		color: [0, 77, 168, 0.80]
+		} )
+	} );
+	quakesRenderer.addClassBreakInfo( {
+  		minValue: 1,
+  		maxValue: 2,
+		label: "1 to 1.9",
+  		symbol: new SimpleMarkerSymbol( {
+    		style: "circle",
+    		size: 14,
+    		color: [0, 77, 168, 0.80]
+		} )
+	} );
+	quakesRenderer.addClassBreakInfo( {
+  		minValue: 2,
+  		maxValue: 3,
+		label: "2 to 2.9",
+  		symbol: new SimpleMarkerSymbol( {
+    		style: "circle",
+    		size: 18,
+    		color: [0, 77, 168, 0.80]
+		} )
+	} );
+	quakesRenderer.addClassBreakInfo( {
+  		minValue: 3,
+  		maxValue: 4,
+		label: "3 to 3.9",
+  		symbol: new SimpleMarkerSymbol( {
+    		style: "circle",
+    		size: 22,
+    		color: [0, 77, 168, 0.80]
+		} )
+	} );
+	quakesRenderer.addClassBreakInfo( {
+  		minValue: 4,
+  		maxValue: 9,
+		label: "4.0 and greater",
+  		symbol: new SimpleMarkerSymbol( {
+    		style: "circle",
+    		size: 26,
+    		color: [0, 77, 168, 0.80]
+		} )
+	} );
+	var quakesLayer = new MapImageLayer( {
+		url: "http://services.kgs.ku.edu/arcgis8/rest/services/tremor/quakes_public/MapServer",
+		sublayers:[ {
+			id: 0,
+			renderer: quakesRenderer
+		} ],
+		id:"KGS Events",
+		visible: true
+	} );
+
+    var map = new Map( {
+		layers: [basemapLayer, quakesLayer]
+    } );
+
+    var view = new MapView( {
         map: map,
-        scalebarUnit: "dual"
-    });
+        container: "map-div",
+        center: [-98, 39.1],
+        zoom: 7,
+        ui: { components: ["zoom"] },
+		constraints: { rotationEnabled: false }
+    } );
 
-    earthquakesLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.kgs.ku.edu/arcgis1/rest/services/co2/seismic_1/MapServer");
-    earthquakesLayer.setVisibleLayers([8]);
-    map.addLayers([earthquakesLayer]);
-});
+    var scaleBar = new ScaleBar( {
+    	view: view,
+    	unit: "dual"
+  	} );
+  	view.ui.add(scaleBar, {
+    	position: "bottom-left"
+  	} );
+} );
 
 function filterQuakes(year, mag) {
     var nextYear = parseInt(year) + 1;
